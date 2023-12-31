a5ae142008d10131984eb3e9937e9a5c795c22d8
okdyy75 <okdyy75@gmail.com>
2023-12-31 21:30:22 +0900

    UserFactory更新

--- www/cakephp/tests/Factory/UserFactory.php
+++ www/cakephp/tests/Factory/UserFactory.php
@@ -36,9 +36,7 @@ class UserFactory extends CakephpBaseFactory
     {
         $this->setDefaultData(function (Generator $faker) {
             return [
-                // set the model's default values
-                // For example:
-                // 'name' => $faker->lastName
+                'name' => $faker->lastName
             ];
         });
     }
